5663ad48bbec206d92d3c024a4611c6fd839d014
Kosala Gunarathna <140527096+KosalaGunarathna@users.noreply.github.com>
2025-10-21 23:41:07 +0530

    crete user routes

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import User from "./models/user.model.js";
+import userRoutes from "./routes/user.routes.js";
  
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 
-app.post("/api/user", async(req, res) => {
-//   res.send("Server is running...");
-    const user = req.body;
-
-    if(!user.username || !user.email || !user.password ) {
-        return res.status(400).json({ message: "All fields are required...." });
-    }
-    try {
-        const newUser = new User(user);
-        await newUser.save();
-        res.status(201).json({ success: true, data: newUser });
-
-    }catch(error) {
-        console.error("Error saving user:", error);
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-    console.log(user.body);
-    res.status(201).json({ message: "User created successfully", user });
-
-});
+app.use("/api/user", userRoutes);
 
 
 
-app.listen(5000, () => {
+
+
+app.listen(process.env.PORT, () => {
     connectDB();
-    console.log("Server is running on http://localhost:5000");
+    console.log("Server is running on http://localhost:"+process.env.PORT);
 } );
 
 
