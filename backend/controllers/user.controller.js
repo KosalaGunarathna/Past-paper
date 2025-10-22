c12fc22531de439cef658832a230b8cfc3efd93b
Kosala Gunarathna <140527096+KosalaGunarathna@users.noreply.github.com>
2025-10-22 18:17:25 +0530

    create user controller

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,68 @@
+import User from "../models/user.model.js";
+import mongoose from "mongoose";
+
+export const getUsers = async (req, res) => {
+    try {
+        const user=await User.find({});
+        res.status(200).json({ success: true, data: user });
+
+    }catch (error){
+        console.log("error find user ",error.message);
+        res.status(200).json({ success: false, message:"Server error "});
+    }
+};
+
+export const registerUser = async (req, res) => {
+    const user = req.body;
+
+    if (!user.username || !user.email || !user.password) {
+        return res.status(400).json({ message: "All fields are required...." });
+    }
+
+    try {
+        const newUser = new User(user);
+        await newUser.save();
+        res.status(201).json({ success: true, data: newUser });
+
+    } catch (error) {
+        console.error("Error saving user:", error);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+    console.log(user.body);
+    res.status(201).json({ message: "User created successfully", user });
+
+};
+
+export const deleteUser = async (req, res) => {
+    // const { id } = req.params;
+    const userDetails = req.body;
+    try {
+        const user = await User.findByIdAndDelete(userDetails.id);
+        res.status(200).json({
+            status: 200,
+            message: "User deleted successfully", 
+            data: user
+
+        });
+    }
+    catch (error) {
+        console.error("Error deleting user:", error);
+        return res.status(500).json({ message: "Server error.." });
+    }
+
+};
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const user = req.body;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(200).json({success:true , message:"Invalid Product Id"});
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id,user,{new:true});
+        return res.status(200).json({success:true , data:updatedUser});
+    } catch (error) {
+        return res.status(500).json({success:false , message:"Server error"});
+    }
+
+};
